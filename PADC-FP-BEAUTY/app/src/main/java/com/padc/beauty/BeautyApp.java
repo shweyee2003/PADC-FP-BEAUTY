3e6b154db58aa497e9f14093529265ce9c45036d
Shwe Yee <shweyee2003@gmail.com>
2016-09-04 15:43:43 +0630

    Initail  Android Project Setup

---
+++ PADC-FP-BEAUTY/app/src/main/java/com/padc/beauty/BeautyApp.java
@@ -0,0 +1,21 @@
+package com.padc.beauty;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by windows on 9/4/2016.
+ */
+public class BeautyApp extends Application {
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
