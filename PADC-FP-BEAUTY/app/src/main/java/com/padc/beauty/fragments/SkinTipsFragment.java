5d3326074025fd7f5fdd7d35b09812ecaaccc7a8
Shwe Yee <shweyee2003@gmail.com>
2016-09-07 22:34:16 +0630

    no message

---
+++ PADC-FP-BEAUTY/app/src/main/java/com/padc/beauty/fragments/SkinTipsFragment.java
@@ -0,0 +1,13 @@
+package com.padc.beauty.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by windows on 9/6/2016.
+ */
+public class SkinTipsFragment extends Fragment {
+    public static SkinTipsFragment newInstance(){
+        SkinTipsFragment skinTipsFragment=new SkinTipsFragment();
+        return skinTipsFragment;
+    }
+}
