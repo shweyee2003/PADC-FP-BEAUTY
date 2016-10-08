c04330f68529b44609c698aacff62654b0d4e471
zinmongnext <zinmonthet88@gmail.com>
2016-10-08 09:47:36 +0630

    repair salon detail

--- PADC-FP-BEAUTY/app/src/main/java/com/padc/beauty/activities/BaseActivity.java
+++ PADC-FP-BEAUTY/app/src/main/java/com/padc/beauty/activities/BaseActivity.java
@@ -1,20 +1,230 @@
 package com.padc.beauty.activities;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ShareCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 
 import com.padc.beauty.R;
+import com.padc.beauty.utils.BeautyAppConstant;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Asus on 10/2/2016.
  */
 public class BaseActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 100;
+    private static final int REQUEST_IMAGE_CAPTURE = 1001;
+    private static final int REQUEST_IMAGE_CAPTURE_FULL_RESOLUTION = 1002;
+    private static final int REQUEST_SELECT_IMAGE_ABOVE_KITKAT = 1003;
+    private static final int REQUEST_SELECT_IMAGE = 1004;
+
+    private String numberToCall = null;
+    private String mCurrentPhotoPath;
+
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        onSendScreenHit();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Bundle extras = data.getExtras();
+                Bitmap takenPicture = (Bitmap) extras.get("data");
+                onPictureTaken(takenPicture);
+            } else if (requestCode == REQUEST_IMAGE_CAPTURE_FULL_RESOLUTION) {
+                onPictureTaken(mCurrentPhotoPath);
+            } else if (requestCode == REQUEST_SELECT_IMAGE_ABOVE_KITKAT) {
+                Uri originalUri = data.getData();
+                final int takeFlags = data.getFlags()
+                        & (Intent.FLAG_GRANT_READ_URI_PERMISSION
+                        | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                // Check for the freshest data.
+                getContentResolver().takePersistableUriPermission(originalUri, takeFlags);
+                //onPictureSelect(originalUri);
+
+                String id = originalUri.getLastPathSegment().split(":")[1];
+                final String[] imageColumns = {MediaStore.Images.Media.DATA};
+                final String imageOrderBy = null;
+
+                Uri uri = getUri();
+
+                String selectedImagePath = "path";
+
+                Cursor imageCursor = managedQuery(uri, imageColumns,
+                        MediaStore.Images.Media._ID + "=" + id, null, imageOrderBy);
+
+                if (imageCursor.moveToFirst()) {
+                    selectedImagePath = imageCursor.getString(imageCursor.getColumnIndex(MediaStore.Images.Media.DATA));
+                }
+
+                onPictureTaken(selectedImagePath);
+            } else if (requestCode == REQUEST_SELECT_IMAGE) {
+                Uri uri = data.getData();
+                String[] projection = {MediaStore.Images.Media.DATA};
+
+                Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    int columnIndex = cursor.getColumnIndex(projection[0]);
+                    String picturePath = cursor.getString(columnIndex); // returns null
+                    cursor.close();
+
+                    onPictureTaken(picturePath);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CALL_PHONE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay!
+                    makeCall(numberToCall);
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    private Uri getUri() {
+        String state = Environment.getExternalStorageState();
+        if (!state.equalsIgnoreCase(Environment.MEDIA_MOUNTED))
+            return MediaStore.Images.Media.INTERNAL_CONTENT_URI;
+
+        return MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+    }
 
     protected void sendViaShareIntent(String msg) {
         startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(BaseActivity.this)
                 .setType("text/plain")
                 .setText(msg)
                 .getIntent(), getString(R.string.action_share)));
+    }
+
+    protected void openLocationInMap(String location) {
+        String uriToOpen = BeautyAppConstant.URI_TO_OPEN_IN_MAP + location;
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uriToOpen));
+        startActivity(intent);
+    }
+
+    protected void makeCall(String numberToCall) {
+        numberToCall = numberToCall.replaceAll(" ", "");
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + numberToCall));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            this.numberToCall = numberToCall;
+
+            /*
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CALL_PHONE)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE);
+
+                // MY_PERMISSIONS_REQUEST_CALL_PHONE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            */
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE);
+
+            return;
+        }
+        startActivity(intent);
+    }
+
+
+    protected void showProgressDialog(String msg) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+        }
+
+        mProgressDialog.setMessage(msg);
+        mProgressDialog.show();
+    }
+
+    protected void dismissProgressDialog() {
+        if(mProgressDialog.isShowing())
+            mProgressDialog.dismiss();
+    }
+
+    public void onPictureTaken(Bitmap takenPicture) {
+
+    }
+
+    public void onPictureTaken(String localPath) {
+
+    }
+
+    protected void onSendScreenHit() {
+
     }
 }
